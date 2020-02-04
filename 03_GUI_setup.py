2aaceb73ebb1faa92c554ea8a95659efd49784ae
Allenr71291 <52386917+Allenr71291@users.noreply.github.com>
2020-02-05 09:55:19 +1300

    GUI

--- 03_GUI_setup.py
+++ 03_GUI_setup.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from functools import partial
 
 class Converter:
     def __init__(self, parent):
@@ -34,6 +34,38 @@ class Help:
 
         # disable help button
         partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (Help Box)
+        self.help_box = Toplevel()
+
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # Sets up GUI Frame
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help / Instructions",
+                                 font="arial 10 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        # Help text (label, row 1)
+        self.help_text = Label(self.help_frame, text="",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss", width=10, bg=background,
+                                  command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+
+
+
 
 # main routine
 if __name__ == "__main__":
